5af5fd0a6bc75fb45244575cd6320bb02b479451
amarabuco <adrianomal@gmail.com>
2023-01-16 17:33:00 -0300

    refactor

--- sqlmodel-tutorial/app.py
+++ sqlmodel-tutorial/app.py
@@ -21,13 +21,12 @@ def create_heroes():
     hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
     hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
     
-    session = Session(engine)
-    
-    session.add(hero_1)
-    session.add(hero_2)
-    session.add(hero_3)
-    
-    session.commit()
+    with Session(engine) as session:
+        session.add(hero_1)
+        session.add(hero_2)
+        session.add(hero_3)
+        
+        session.commit()
 
 def main():
     create_db_and_tables()
