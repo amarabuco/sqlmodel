dcf9c3b2a187d914442881fa4d508b80e038e279
amarabuco <adrianomal@gmail.com>
2023-01-16 17:47:55 -0300

    refactor

--- sqlmodel-tutorial/app.py
+++ sqlmodel-tutorial/app.py
@@ -32,8 +32,8 @@ def select_heroes():
     with Session(engine) as session:
         statement =  select(Hero)
         results = session.exec(statement)
-        for hero in results:
-            print(hero)
+        heroes = results.all()
+        print(heroes)
         
 
 def main():
