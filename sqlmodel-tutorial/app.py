2703f4532177a2c8b4376252139638f87612a9d8
amarabuco <adrianomal@gmail.com>
2023-01-16 17:11:09 -0300

    refactor

--- sqlmodel-tutorial/app.py
+++ sqlmodel-tutorial/app.py
@@ -13,4 +13,8 @@ sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 engine = create_engine(sqlite_url, echo=True)
 
-SQLModel.metadata.create_all(engine)
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+    
+if __name__ == "__main__":
+    create_db_and_tables()
