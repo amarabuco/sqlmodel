6584d7f663992185a0e3e0213c1f6ee9928bdd44
amarabuco <adrianomal@gmail.com>
2023-01-16 22:23:39 -0300

    Remove Relationships

--- sqlmodel-tutorial/app.py
+++ sqlmodel-tutorial/app.py
@@ -117,12 +117,26 @@ def list_heroes():
         team_preventers = result.one()
 
         print("Preventers heroes:", team_preventers.heroes)
+        
+def update_heroes():
+    with Session(engine) as session:
+        statement = select(Hero).where(Hero.name == "Spider-Boy")
+        result = session.exec(statement)
+        hero_spider_boy = result.one()
+
+        hero_spider_boy.team = None # set team None
+        session.add(hero_spider_boy)
+        session.commit()
+
+        session.refresh(hero_spider_boy)
+        print("Spider-Boy without team:", hero_spider_boy)
     
 def main():
     create_db_and_tables()
     create_heroes()
     select_heroes()
     list_heroes()
+    update_heroes()
     
 if __name__ == "__main__":
     main()
